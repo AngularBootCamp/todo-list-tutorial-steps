8d10fcc126494099240bb0d099e85f6039c920bf
Bill Odom <bill@billodom.com>
2018-04-16 05:24:56 -0600

    Complete "Service" step

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoListService } from '../todo-list.service';
+
 @Component({
   selector: 'todo-list-manager',
   template: `
@@ -20,22 +22,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
+  title = 'todo';
+  todoList;
 
-  constructor() { }
+  constructor(private todoListService:TodoListService) { }
 
   ngOnInit() {
+    this.todoList = this.todoListService.getTodoList();
   }
-
-  title = 'todo';
-
-  todoList = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
 
   addItem(title: string): void {
     this.todoList.push({ title });

ef46ff7eb0a741d2c8ba35383dc36174bf32594c
Bill Odom <bill@billodom.com>
2018-04-16 06:43:14 -0600

    Complete "Adding a checkbox" step

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -4,11 +4,19 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   selector: 'todo-item',
   template: `
     <div class="todo-item">
-      {{ todoItem.title }}
+
+      <input type="checkbox"
+             class="todo-checkbox"
+             (click)="completeItem()">
+
+      <span class="todo-title" [ngClass]="{'todo-complete': isComplete}">
+        {{ todoItem.title }}
+      </span>
 
       <button class="btn btn-red" (click)="removeItem()">
         remove
       </button>
+
     </div>
   `,
   styleUrls: ['./item.component.css']
@@ -17,6 +25,8 @@ export class ItemComponent implements OnInit {
   @Input() todoItem: any;
   @Output() remove:EventEmitter<any> = new EventEmitter();
 
+  isComplete: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
@@ -24,5 +34,9 @@ export class ItemComponent implements OnInit {
 
   removeItem() {
     this.remove.emit(this.todoItem);
+  }
+
+  completeItem() {
+    this.isComplete = !this.isComplete;
   }
 }

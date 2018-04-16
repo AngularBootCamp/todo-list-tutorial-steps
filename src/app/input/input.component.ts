637643c1e0dd284b58b514f8e1f65b39e8ae8147
Bill Odom <bill@billodom.com>
2018-04-16 04:12:11 -0600

    Complete "Event binding" step

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -3,8 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-input',
   template: `
-    <input [value]="title">
-    <button>Save</button>
+    <input [value]="title"
+          (keyup.enter)="changeTitle($event.target.value)">
+    <button (click)="changeTitle('Button Clicked!')">
+      Save
+    </button>
     <p>The title is: {{ title }}</p>
   `,
   styleUrls: ['./input.component.css']
